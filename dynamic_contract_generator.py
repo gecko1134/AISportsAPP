7bc6c8ebead60fe5ed4a44c6b4426b2b4c9f7849
gecko1134 <85382061+gecko1134@users.noreply.github.com>
2025-05-20 08:39:09 -0500

    Add files via upload

--- dynamic_contract_generator.py
+++ dynamic_contract_generator.py
@@ -1,5 +1,1 @@
-from pandadoc import PandaDocClient
-
-def generate_contract(template_id: str, data: dict, api_key: str) -> dict:
-    client = PandaDocClient(api_key)
-    return client.create_document_from_template(template_id, data)
+def generate_contract(): pass
